e080d2215db1dd3fda4efb2eb9b5790d0a76e91f
reon777 <r.saito1226@gmail.com>
2018-05-22 07:37:02 +0900

    パラメータ総当たり&gini評価

--- protos/train.py
+++ protos/train.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import log_loss, roc_auc_score
+from sklearn.model_selection import StratifiedKFold, ParameterGrid
+from sklearn.metrics import log_loss, roc_auc_score, roc_curve, auc
 from load_data import load_train_data, load_test_data
 
 logger = getLogger(__name__)
 
 DIR = 'result_tmp/'
 SAMPLE_SUBMIT_FILE = '../input/sample_submission.csv'
+
+def gini(y, pred):
+    fpr, tpr, thr = roc_curve(y, pred, pos_label=1)
+    g = 2 * auc(fpr, tpr) -1
+    return g
 
 if __name__ =='__main__':
 
@@ -45,33 +51,58 @@ if __name__ =='__main__':
 
     # ------------学習----------------------
 
-    # クロスバリデーション
+    # クロスバリデーションの設定
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
-    list_auc_score = []
-    list_logloss_score = []
 
-    for train_idx, valid_idx in cv.split(x_train, y_train):
-        trn_x = x_train.iloc[train_idx, :]
-        val_x = x_train.iloc[valid_idx, :]
+    # 総当たり検索対象のパラメータ一覧
+    all_param = {'C': [10**i for i in range(-1, 2)],
+                 'fit_intercept': [True, False],
+                 'penalty': ['l2', 'l1'],
+                 'random_state': [0]
+                    }
+    min_score = 100
+    min_params = None
+    # パラメータ総当たり
+    for params in tqdm(list(ParameterGrid(all_param))):
+        logger.info('params: {}'.format(params))
+        list_logloss_score = []
+        list_gini_score = []
 
-        trn_y = y_train[train_idx]
-        val_y = y_train[valid_idx]
+        # クロスバリデーション
+        for train_idx, valid_idx in cv.split(x_train, y_train):
+            trn_x = x_train.iloc[train_idx, :]
+            val_x = x_train.iloc[valid_idx, :]
 
-        clf = LogisticRegression(random_state=0)
-        clf.fit(trn_x, trn_y)
+            trn_y = y_train[train_idx]
+            val_y = y_train[valid_idx]
 
-        pred = clf.predict_proba(val_x)[:, 1]
-        sc_logloss = log_loss(val_y, pred)
-        sc_auc = roc_auc_score(val_y, pred)
+            clf = LogisticRegression(**params)
+            clf.fit(trn_x, trn_y)
 
-        list_logloss_score.append(sc_logloss)
-        list_auc_score.append(sc_auc)
-        logger.debug('    logloss: {}, auc: {}'.format(sc_logloss, sc_auc))
+            pred = clf.predict_proba(val_x)[:, 1]
+            sc_logloss = log_loss(val_y, pred)
+            sc_gini = - gini(val_y, pred)
 
-    logger.info('loglos: {}, auc: {}'.format(np.mean(list_logloss_score), np.mean(list_auc_score)))
+            list_logloss_score.append(sc_logloss)
+            list_gini_score.append(sc_gini)
+            logger.debug('    logloss: {}, gini: {}'.format(sc_logloss, sc_gini))
 
-    # random_stateに数字を指定することで結果を再現できる
-    clf = LogisticRegression(random_state=0)
+        # クロスバリデーション結果の平均を取る
+        sc_logloss = np.mean(list_logloss_score)
+        sc_gini = - np.mean(list_gini_score)
+        # パラメータ総当たり中にスコアを更新していく
+        if min_score > sc_gini:
+            min_score = sc_gini
+            min_params = params
+        logger.info('current loglos: {}, gini: {}'.format(sc_logloss, sc_gini))
+        logger.info('current min_score: {}, min_params: {}'.format(min_score, min_params))
+
+    # パラメータ総当たりで最も良いパラメータとスコア
+    logger.info('min_params: {}'.format(min_params))
+    logger.info('min_score: {}'.format(sc_gini))
+
+    # もっとも良いパラメータでトレーニングする
+    clf = LogisticRegression(**min_params)
     clf.fit(x_train, y_train)
     logger.info('train end')
 
